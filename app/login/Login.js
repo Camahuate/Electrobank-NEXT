0887d7f0f97276e7665e13161928abcaf6ee7d1b
Ayax Garcia Williner <agarciawilliner@gmail.com>
2023-09-21 23:57:41 -0300

    Agregue tarjetas

--- app/login/Login.js
+++ app/login/Login.js
@@ -1,51 +1,50 @@
-import React from "react"
+import React from "react";
 import { useState } from "react";
 import Image from 'next/image';
 import '../main/styles/Login.css';
 
-const Login = ({setIsLogged}) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-  
-    const handleLogin = () => {
-      if (email === 'usuario@example.com' && password === 'password') {
-        setIsLogged(true);
-      } else {
-        alert('Credenciales incorrectas');
-      }
-    };
-  
-    const handleLogout = () => {
-      setIsLogged(false);
-      setEmail('');
-      setPassword('');
-    };
+const Login = ({ setIsLogged }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    return (
-        <div className='body-login'>
-              <div className='login-container'>
-                <div className='form-login'>
-                <Image  className="logo" src='/ELECTOBANK-logo.png' alt="logobank" sizes="max-width: 200px, " width={200} height={24}
-              priority/>
-                <h2>Iniciar sesión</h2>
-                <input
-                  type="email"
-                  placeholder="Correo electrónico"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                  type="password"
-                  placeholder="Contraseña"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <br/>
-                <button className='btn-violeta' onClick={handleLogin}>Iniciar sesión</button>
-                </div>
-              </div>
+  const handleLogin = () => {
+    if (email === 'usuario@example.com' && password === 'password') {
+      setIsLogged(true);
+    } else {
+      alert('Credenciales incorrectas');
+    }
+  };
+
+  const handleLogout = () => {
+    setIsLogged(false);
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div className='body-login'>
+      <div className='login-container'>
+        <div className='form-login'>
+          <Image className="logo" src='/ELECTOBANK-logo.png' alt="logobank" sizes="max-width: 200px, " class="centered-image" width={200} height={24} priority />
+          <h2>Iniciar sesión</h2>
+          <input
+            type="email"
+            placeholder="Correo electrónico"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <button className='btn-violeta' onClick={handleLogin}>Iniciar sesión</button>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Login;
--- app/main/MenuLateral.js
+++ app/main/MenuLateral.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import './styles/MenuLateral.css';
-import { Link } from "react-router-dom";
+import Link from 'next/link'; // Importa Link de Next.js
+
 function MenuLateral() {
   return (
     <div className="menu-lateral">
       <ul>
-            <li>
-              <Link to="/">Inicio</Link>
-            </li>
-            <li>
-              <Link to="/transferencias">Transferencias</Link>
-            </li>
-            <li>
-              <Link to="/pagos">Pagos</Link>
-            </li>
-            <li>
-              <Link to="/cuentas">Cuentas</Link>
-            </li>
+        <li>
+          <Link href="/">Inicio</Link>
+        </li>
+        <li>
+          <Link href="/transferencias">Transferencias</Link>
+        </li>
+        <li>
+          <Link href="/pagos">Pagos</Link>
+        </li>
+        <li>
+          <Link href="/cuentas">Cuentas</Link>
+        </li>
+        <li>
+          <Link href="/main/tarjetas">Tarjetas</Link>
+        </li>
       </ul>
     </div>
   );
